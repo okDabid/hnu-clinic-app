e36a8f9c589249aa388a919b598f1e6714266944
okDabid <matthewmaniwang@gmail.com>
2025-09-21 17:02:22 +0800

    Added admin create users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,16 @@
-import Image from "next/image";
 
-export default function Home() {
+import NurseForm from "./components/NurseForm";
 
+export default function HomePage() {
   return (
-    <h1>Hello World</h1>
+    <main className="p-6">
+      <h1 className="text-3xl font-bold text-green-600 mb-6">
+        Admin Dashboard
+      </h1>
+
+      <div className="max-w-2xl mx-auto">
+        <NurseForm />
+      </div>
+    </main>
   );
 }
