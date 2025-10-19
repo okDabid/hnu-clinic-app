e11fe605f15f1ffd46b4bd6b8573196d0d3d3c8a
okDabid <matthewmaniwang@gmail.com>
2025-10-19 21:33:10 +0800

    ggg v3

--- src/app/login/LoginPageClient.tsx
+++ src/app/login/LoginPageClient.tsx
@@ -142,7 +142,7 @@ export default function LoginPageClient() {
             });
             const data = await res.json();
             if (res.ok) {
-                toast.success("Verification code sent via Email/SMS!");
+                toast.success("Verification code sent via Emai!");
                 setTokenSent(true);
                 setResendCooldown(60);
                 return true;
