6efbdbaf7cb3e774f2ae8ba220866e7531220f2d
okDabid <matthewmaniwang@gmail.com>
2025-11-20 08:33:25 +0800

    Updating packages

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,8 +1,8 @@
+import "dotenv/config";
 import { defineConfig, env } from "@prisma/config";
 
 export default defineConfig({
     schema: "./prisma/schema.prisma",
-    engine: "classic",
     datasource: {
         url: env("DATABASE_URL"),
     },
