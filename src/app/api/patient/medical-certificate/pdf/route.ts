6c0a639794b07cec90a1053b619a553d45b97e34
okDabid <matthewmaniwang@gmail.com>
2025-12-03 20:06:54 +0800

    Changing attechment to inline in pdf generation

--- src/app/api/patient/medical-certificate/pdf/route.ts
+++ src/app/api/patient/medical-certificate/pdf/route.ts
@@ -59,7 +59,7 @@ export async function GET() {
             status: 200,
             headers: {
                 "Content-Type": "application/pdf",
-                "Content-Disposition": `attachment; filename="${filename}"`,
+                "Content-Disposition": `inline;  filename="${filename}"`,
                 "Cache-Control": "no-store",
             },
         });
