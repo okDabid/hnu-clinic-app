f9f91bc0b5d5b8903d49ebd1983352bae7cd1b2c
okDabid <matthewmaniwang@gmail.com>
2025-12-04 22:24:54 +0800

    Updating next config

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@sparticuz/chromium"],
+  },
+  outputFileTracingIncludes: {
+    "/api/doctor/appointments/[id]/certificate": [
+      "./node_modules/@sparticuz/chromium/**/*",
+    ],
+    "/api/nurse/reports/pdf": ["./node_modules/@sparticuz/chromium/**/*"],
+  },
   allowedDevOrigins: [
     "http://192.168.254.104:3000",
     "http://localhost:3000",
