1b468e8ed71bfce93dcc999dd0fe98fe0ce54a5e
okDabid <matthewmaniwang@gmail.com>
2025-12-05 23:45:24 +0800

    Removing puppeter experimental config

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
 
-  serverExternalPackages: ["@sparticuz/chromium"],
-
-  experimental: {
-
-  },
-  outputFileTracingIncludes: {
-    "/api/doctor/appointments/[id]/certificate": [
-      "./node_modules/@sparticuz/chromium/**/*",
-    ],
-    "/api/nurse/reports/pdf": ["./node_modules/@sparticuz/chromium/**/*"],
-  },
-
   allowedDevOrigins: [
     "http://192.168.254.104:3000",
     "http://localhost:3000",
