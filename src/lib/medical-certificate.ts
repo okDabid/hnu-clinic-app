8378e70c72052e2a8f35d60eba67492ad0d13c73
okDabid <matthewmaniwang@gmail.com>
2025-12-06 23:40:46 +0800

    Update v2

--- src/lib/medical-certificate.ts
+++ src/lib/medical-certificate.ts
@@ -319,18 +319,8 @@ export function renderCertificateHtml(context: CertificateContext) {
         position: relative;
       }
 
-      .checkbox.checked .box {
-        content: "🗹";
-        position: absolute;
-        inset: 0;
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        font-size: 12px;
-        line-height: 1;
-      }
 
-      .checkbox.checked .box::after {
+      .checkbox.checked .box {
         content: "🗹";
         position: absolute;
         inset: 0;
